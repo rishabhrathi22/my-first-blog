98bae41a567ac1bfeefb9f3f4ceeeb15b610fd09
rishabhrathi22 <43409681+rishabhrathi22@users.noreply.github.com>
2019-07-24 17:36:11 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 0f8e60a2bcf1775c200795998d014efdc7acc074
+
